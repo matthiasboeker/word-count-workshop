e521ec62168ab4f0fba64097f4bb13808fdb35e1
Matthias Boeker <matthias.boeker@icloud.com>
2024-03-19 17:04:09 +0100

    change some

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-mkdir -p ${RESULTSDIR}
-mkdir -p ${FIGDIR}
-
 for title in abyss isles last sierra; do
     python3 code/count.py ${DATADIR}/${title}.txt -o ${RESULTSDIR}/${title}.json
     python code/plot.py ${RESULTSDIR}/${title}.json -o ${FIGDIR}/${title}.png
